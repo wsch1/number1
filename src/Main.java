d1445b1a6e62164386fb14f4c18e5caef0f10763
Sergei Demin <wsch@bk.ru>
2022-10-19 18:21:29 +0300

    first

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int priceTicket = 1000;  //стоимость билета
+        int priceBonus = 20;
+        int bonusMils = priceTicket / priceBonus;
+
+        System.out.println(bonusMils);
+    }
+}
